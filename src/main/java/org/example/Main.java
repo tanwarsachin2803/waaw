b325e125a42d4ed9381b5a5163dbd70974820ec1
tanwarsachin2803 <tanwarsachin2803@gmail.com>
2024-01-23 21:45:29 -0500

    This is code
    code change

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,25 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int[] a={1,2,5,10,12,14};
+        int[] result=new int[2];
+        int target=12;
+        for(int i=0;i<a.length;i++)
+        {
+            for(int j=i+1;j<a.length;j++)
+            {
+                int sum=a[i]+a[j];
+                if(sum==target)
+                {
+                    result[0]=i;
+                    result[1]=j;
+                }
+            }
+        }
+        for(int i=0;i<result.length;i++)
+        {
+            System.out.print(result[i]+" ");
+        }
     }
 }
