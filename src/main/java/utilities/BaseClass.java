d652a088b669c50c18a8202733a68a046b17d7a8
tanwarsachin2803 <tanwarsachin2803@gmail.com>
2024-01-25 21:28:26 -0500

    base_package
    base_package and base class is going to be commited in this change

---
+++ src/main/java/utilities/BaseClass.java
@@ -0,0 +1,23 @@
+package utilities;
+
+public class BaseClass {
+
+    //method to select the browser
+
+    //method to open that browser by using WebDriverManager
+
+    // method to open the url of tht waaw project
+
+
+
+
+
+    // git branch "branch_name"
+    // git branch
+    // git checkout "branch_name"
+
+
+    // git add filenames
+    // git commit -m "message_name"
+    // create pr from ur git hub
+}
