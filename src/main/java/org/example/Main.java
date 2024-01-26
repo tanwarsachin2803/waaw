2adb11ebd7e4917ffdf33663cbc61217c2bfbf4d
jenniferfernando86 <jennifer.fernando86@gmail.com>
2024-01-25 23:33:02 -0500

    Base files added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,22 @@
 package org.example;
 
+import org.openqa.selenium.WebDriver;
+import utilities.BaseClass;
+
+import java.time.Duration;
+
+import static utilities.BaseClass.selectBrowser;
+
 public class Main {
     public static void main(String[] args) {
 
-        int[] a={1,2,5,10,12,14};
+        WebDriver driver = selectBrowser("chrome");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.get("https://staging.waaw.ca/");
+        // Perform your tests...
+
+        driver.quit();
+        /*int[] a={1,2,5,10,12,14};
         int[] result=new int[2];
         int target=12;
         for(int i=0;i<a.length;i++)
@@ -21,6 +34,6 @@ public class Main {
         for(int i=0;i<result.length;i++)
         {
             System.out.print(result[i]+" ");
-        }
+        }*/
     }
 }
--- src/main/java/utilities/BaseClass.java
+++ src/main/java/utilities/BaseClass.java
@@ -1,6 +1,43 @@
 package utilities;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
 public class BaseClass {
+
+    public static WebDriver selectBrowser(String browserName){
+        WebDriver driver;
+
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                System.setProperty("webDriver.chrome.driver", "C:/Program Files/Google/Chrome/Application/chrome.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webDriver.gecko.driver", "path/to/geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            case "safari":
+                driver = new SafariDriver();
+                break;
+
+            default:
+                throw new IllegalArgumentException("Unsupported browser: " + browserName);
+
+        }
+        return driver;
+
+    }
+
+
+}
+
+
 
     // total we are 10 guy, we are going to change in our local machine
 
@@ -23,4 +60,5 @@ public class BaseClass {
     // git add filenames
     // git commit -m "message_name"
     // create pr from ur git hub
-}
+
+
