062aa8dc5fe8839f68d52c61c528cf65cb04cfb7
khokher-poonam <khokherpoonam13@gmail.com>
2024-02-01 11:56:07 -0500

    new changes

--- src/main/java/com/qa/waaw/Pages/LoginPage.java
+++ src/main/java/com/qa/waaw/Pages/LoginPage.java
@@ -1,5 +1,6 @@
 package com.qa.waaw.Pages;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.qa.waaw.Utilities.BaseClass;
 
 import java.io.IOException;
@@ -7,5 +8,6 @@ import java.io.IOException;
 public class LoginPage extends BaseClass {
 
     public LoginPage() throws IOException {
+
     }
 }
---
+++ src/test/java/com/qa/waaw/TestCases/LoginTest.java
@@ -0,0 +1,18 @@
+package com.qa.waaw.TestCases;
+
+import com.qa.waaw.Utilities.BaseClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class LoginTest  extends BaseClass {
+
+    public LoginTest() throws IOException {
+    }
+
+    @Test
+    public void url()
+    {
+initializeBrowser();
+    }
+}
